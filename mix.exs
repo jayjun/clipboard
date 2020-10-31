2ea394a5cf54bd8ae3c6e189635c46c19e8aea1a
Tan Jay Jun <tan@jayjun.com>
2020-10-31 19:27:41 +0800

    Format everything

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Clipboard.Mixfile do
       description: "Copy and paste from system clipboard",
       deps: deps(),
       package: package(),
-      source_url: @github_url,
+      source_url: @github_url
     ]
   end
 
@@ -23,7 +23,7 @@ defmodule Clipboard.Mixfile do
   defp deps() do
     [
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
 
@@ -31,7 +31,7 @@ defmodule Clipboard.Mixfile do
     [
       maintainers: ["Tan Jay Jun"],
       licenses: ["MIT"],
-      links: %{"GitHub" => @github_url},
+      links: %{"GitHub" => @github_url}
     ]
   end
 end
